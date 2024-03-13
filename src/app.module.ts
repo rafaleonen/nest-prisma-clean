a589fdf3c149615aba15b0dccc33b457681ae102
Rafael Leonen <leonen.rafael@gmail.com>
2024-03-13 14:12:18 -0300

    feat: es lint, prisma, zod, docker postgres and create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { CreateAccountController } from './controllers/create-account-controller'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
 })
 export class AppModule {}
